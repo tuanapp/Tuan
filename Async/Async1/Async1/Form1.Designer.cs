fd18142fb91ee71b156a9f3eae49d57e9fc2c188
tuanapp <kdx.shaggy@gmail.com>
2014-12-31 18:07:33 +0530

    Async/Await/Task
    Can implement asyncronous functionality. No need to use callback
    functions.

---
+++ Async/Async1/Async1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Async1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCallAsync = new System.Windows.Forms.Button();
+            this.btnCallAsync2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCallAsync
+            // 
+            this.btnCallAsync.Location = new System.Drawing.Point(79, 37);
+            this.btnCallAsync.Name = "btnCallAsync";
+            this.btnCallAsync.Size = new System.Drawing.Size(114, 62);
+            this.btnCallAsync.TabIndex = 0;
+            this.btnCallAsync.Text = "Call Async";
+            this.btnCallAsync.UseVisualStyleBackColor = true;
+            this.btnCallAsync.Click += new System.EventHandler(this.btnCallAsync_Click);
+            // 
+            // btnCallAsync2
+            // 
+            this.btnCallAsync2.Location = new System.Drawing.Point(79, 118);
+            this.btnCallAsync2.Name = "btnCallAsync2";
+            this.btnCallAsync2.Size = new System.Drawing.Size(114, 62);
+            this.btnCallAsync2.TabIndex = 0;
+            this.btnCallAsync2.Text = "Call Async 2";
+            this.btnCallAsync2.UseVisualStyleBackColor = true;
+            this.btnCallAsync2.Click += new System.EventHandler(this.btnCallAsync2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(290, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(365, 251);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 681);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCallAsync2);
+            this.Controls.Add(this.btnCallAsync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCallAsync;
+        private System.Windows.Forms.Button btnCallAsync2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Async/Async1/Async1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCallAsync_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(await GetMessage("Task 1"));
+        }
+
+        private static Task<string> GetMessage(string msg)
+        {
+            return Task.Run(() =>
+                {
+                    int i = 0;
+                    while (i < 5)
+                    {
+                        Thread.Sleep(1000);
+                        i++;
+                    }
+                    return msg + " Complete.";
+                });
+        }
+
+        private async void btnCallAsync2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(await GetMessage("Task 2"));
+        }
+    }
+}
